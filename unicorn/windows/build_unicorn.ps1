0525a43050a0603406a5e6a42b6d0df7f9749e15
Luiz Scheinkman <lscheinkman@numenta.com>
2016-06-27 17:03:07 -0700

    UNI-319: Fix 'command line is too long' when using 'winrm'. Introduce 'build.properties' file to inject late env vars like 'CSC_LINK'

--- unicorn/windows/build_unicorn.ps1
+++ unicorn/windows/build_unicorn.ps1
@@ -19,18 +19,17 @@
 #
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
-# Script used to build win32 version of Unicorn.
-# Arguments:
+# Script used to build windows version of Unicorn.
+# Environment Variables:
 #   1) CSC_LINK. See https://github.com/electron-userland/electron-builder/wiki/Code-Signing
 #   2) CSC_KEY_PASSWORD. See https://github.com/electron-userland/electron-builder/wiki/Code-Signing
 #   3) GA_TRACKING_ID. See https://analytics.google.com/analytics
+#
+# These variables may also be declared in a local file called 'build.properties'. For example:
+# CSC_LINK=ABCDEFGabcdefg12345678
+# CSC_KEY_PASSWORD=Password
+# GA_TRACKING_ID=UA-99999999-9
 #>
-param (
-    [string]$nupic_version = "0.5.0",
-    [string]$CSC_LINK = "",
-    [string]$CSC_KEY_PASSWORD = "",
-    [string]$GA_TRACKING_ID = ""
-)
 Write-Host "Configure WinRM"
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 
@@ -39,13 +38,17 @@ Write-Host "Mount shared folder to 'x:' drive"
 # See 'config.vm.synced_folder')
 net use x: \\VBOXSVR\shared /PERSISTENT:YES
 
-Write-Host "Set environment variables"
-$env:CSC_LINK = $CSC_LINK
-$env:CSC_KEY_PASSWORD = $CSC_KEY_PASSWORD
-$env:GA_TRACKING_ID = $GA_TRACKING_ID
+Write-Host "Get variables from '$PSScriptRoot\build.properties'"
+if (Test-Path $PSScriptRoot\build.properties) {
+  Select-String '^([^=]*)=(.*)$' $PSScriptRoot\build.properties | % {
+    $name  = $_.Matches.Groups[1].Value
+    $value = $_.Matches.Groups[2].Value
+    Write-Host "Updating variable " $name
+    Set-Item -Path Env:\$name -Value $value
+  }
+}
 
-Write-Host "Clean, install, build and package windows version"
+Write-Host "Build and package windows version"
 pushd x:\
-npm run clean
 npm run build:win
 popd
